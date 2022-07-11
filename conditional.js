4daa4e7e413f64dee6e7f4434c653297a4934178
+ <+>
2022-07-11 12:05:34 -0400

    first file

---
+++ conditional.js
@@ -0,0 +1,85 @@
+/*
+const name = "Alice";
+let greeting;
+
+switch (name) {
+  case "Alice":
+    greeting = "Hello, Alice!";
+    break;
+  case "The White Rabbit":
+    greeting = "Don't be late, White Rabbit";
+    break;
+  case "The Mad Hatter":
+    greeting = "Welcome to the tea party, Mad Hatter";
+    break;
+  case "The Queen of Hearts":
+    greeting = "Please don't chop off my head!";
+    break;
+  default:
+    greeting = "Whoooo are you?";
+}
+
+console.log(greeting);
+/////////////////////////////////////////
+
+const name = "Grumpy";
+let characterType;
+
+switch (name) {
+  case "Sleepy":
+  case "Sneezy":
+  case "Happy":
+  case "Grumpy":
+  case "Bashful":
+  case "Dopey":
+  case "Doc":
+    characterType = "dwarf";
+    break;
+  case "Handsome Prince":
+    characterType = "hero";
+    break;
+  case "Evil Queen":
+    characterType = "villain";
+    break;
+  case "Snow White":
+    characterType = "heroine";
+    break;
+  default:
+    characterType = "minor character";
+}
+
+console.log(characterType);
+*/
+
+
+const age = 21;
+let isAdult, canWork, canEnlist, canDrink;
+
+switch (true) {
+  case age >= 21:
+    canDrink = true;
+  case age >= 18:
+    isAdult = true;
+    canEnlist = true;
+  case age >= 16:
+    canWork = true;
+}
+console.log(canWork);
+console.log(canEnlist);
+console.log(isAdult);
+console.log(canDrink);
+
+/*const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+*/
