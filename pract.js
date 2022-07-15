9770db7af09997f5c1165462c05343130f51002e
+ <+>
2022-07-15 17:03:46 -0400

    ArrayLab

--- pract.js
+++
@@ -1,5 +0,0 @@
-function sayHiToHeadphonedRoommate() {
-  if (sayHiToHeadphonedRoommate.toLowerCase){return("I can\'t hear you!");}
-  if (sayHiToHeadphonedRoommate.toUpperCase) {return("YES INDEED!");}
-  else{return "I would love to!"}
-}
