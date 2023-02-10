2ae72606faa0d667925af0a15513015ad6e496f3
+ <lmcjcr@gmail.com>
2023-02-10 08:40:05 -0500

    application

---
+++ JavascriptMosh/JS-Basics/rangePriceObj.js
@@ -0,0 +1,13 @@
+// create an obj we called priceRangeObject
+//create an [] with 3 obj each ojg it should have 4 properties
+//label tooltip, min and max price meal per person between 0 and 10 dollars
+let priceRanges = [
+  { label: '$', toootip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+  { label: '$$', toootip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+  { label: '$$$', toootip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+];
+// this is inportant for filtering
+//if you building an application for restaurant somewere need to store the list of restaurants.
+let restaurant = [
+  { averagePerPerson: 5 }
+]
